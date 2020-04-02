9c7714f4215a048c00d2af67f24485f543e579bf
raikm <raik.mueller@copra-system.de>
2020-04-02 17:37:48 +0200

    impl Basic Django Server
    - recieve multiple Data and save them (overwrites them)
    - basic REST API for POST impl

---
+++ Django_Server/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/Macintosh HDD/Benutzer/RaikMueller/RecruitmentTool_Backend/recruitmenttool/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
