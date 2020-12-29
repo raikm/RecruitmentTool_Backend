4d3b3cee0099cb061b208ef32fee23aa04bfff95
Raik <raik.mueller@copra-system.de>
2020-12-29 14:25:10 +0100

    New Unit Test for XpathEvaluator

--- Django_Server/recruitmenttool/cdamanager/UnitTests/TestXpathEvaluator.py
+++ Django_Server/recruitmenttool/cdamanager/UnitTests/TestXpathEvaluator.py
@@ -1,20 +1,33 @@
 import unittest
-import Django_Server.recruitmenttool.cdamanager.XpathEvaluator
+from Django_Server.recruitmenttool.cdamanager.XpathEvaluator import XpathEvaluator
 
 
 class TestXpathEvaluator(unittest.TestCase):
 
+    def setUp(self):
+        self.xpath_valid = """//observation[templateId/@root="1.2.40.0.34.11.1.3.6"][code/@codeSystem='2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName"""
+        self.xpath_no_valid = """//[templateId/@root="1.2.40.0.34.11.1.3.6"][code/@codeSystem='2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName"""
+        self.xpath_valid_with_space = """//observation[templateId/@root = "1.2.40.0.34.11.1.3.6"][code/@codeSystem = '2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName"""
+        self.xpath_valid_with_space_at_end = """//observation[templateId/@root = "1.2.40.0.34.11.1.3.6"][code/@codeSystem = '2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName               """
     def test_validate_xpath__with_valide_xpath(self):
-        pass
+        result = XpathEvaluator.validate_xpath(XpathEvaluator, xpath=self.xpath_valid)
+        self.assertEqual("""//observation[templateId/@root="1.2.40.0.34.11.1.3.6"][code/@codeSystem='2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName""", result)
+
 
     def test_validate_xpath__with_no_valide_xpath(self):
-        pass
+        result = XpathEvaluator.validate_xpath(XpathEvaluator, xpath=self.xpath_no_valid)
+        self.assertEqual("", result)
+
 
     def test_remove_blankspaces(self):
-        pass
+        result = XpathEvaluator.validate_xpath(XpathEvaluator, xpath=self.xpath_valid_with_space)
+        self.assertEqual("""//observation[templateId/@root="1.2.40.0.34.11.1.3.6"][code/@codeSystem='2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName""", result)
 
     def test_remove_ten_blankspaces(self):
-        pass
+        result = XpathEvaluator.validate_xpath(XpathEvaluator, xpath=self.xpath_valid_with_space_at_end)
+        self.assertEqual(
+            """//observation[templateId/@root="1.2.40.0.34.11.1.3.6"][code/@codeSystem='2.16.840.1.113883.6.96'andcode/@code='282291009'andstarts-with(value/@code,"M25.46")]/value/@displayName""",
+            result)
 
 
     if __name__ == '__main__':
--- Django_Server/recruitmenttool/cdamanager/XpathEvaluator.py
+++ Django_Server/recruitmenttool/cdamanager/XpathEvaluator.py
@@ -4,11 +4,14 @@ import lxml.etree
 class XpathEvaluator:
 
     def validate_xpath(self, xpath):
-        # if syntax wrong then exception raised
-        lxml.etree.XPath(xpath)
+        try:
+            # if syntax wrong then exception raised
+            lxml.etree.XPath(xpath)
 
-        validated_xpath = self.remove_blankspaces(xpath)
-        return validated_xpath
+            validated_xpath = self.remove_blankspaces(XpathEvaluator, xpath)
+            return validated_xpath
+        except lxml.etree.XPathSyntaxError:
+            return ""
 
     def remove_blankspaces(self, xpath):
         return xpath.replace(" ", "")
